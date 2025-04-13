f49d656784e0550689b2ecdb319a23e4554b7e6e
RafAigue <rafa.aigue@gmail.com>
2025-04-13 12:24:07 +0100

    City selector and Warning components

---
+++ components/CitySelector.js
@@ -0,0 +1,36 @@
+import { Text, Pressable, StyleSheet } from "react-native";
+import { MapPinIcon } from "react-native-heroicons/outline";
+
+export default function CitySelector({
+  city,
+  setSelectedLocation,
+  setShowCities,
+}) {
+  return (
+    <Pressable
+      style={styles.citySelector}
+      onPress={() => {
+        setSelectedLocation(city);
+        setShowCities(false);
+      }}
+    >
+      <MapPinIcon size={15} style={styles.icon} />
+      <Text style={styles.city}>{city.name}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  citySelector: {
+    display: "flex",
+    flexDirection: "row",
+    marginBottom: 1,
+    paddingVertical: 8,
+    paddingLeft: 10,
+    backgroundColor: "#f3feffe6",
+    color: "#000",
+    gap: 5,
+  },
+  icon: { color: "blue", marginRight: 2 },
+  city: { position: "relative", color: "black" },
+});

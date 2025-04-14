2551ebb159d7844a2f5c1044c2737a0dbea1b9b9
RafAigue <rafa.aigue@gmail.com>
2025-04-14 12:35:35 +0100

    Added network status control + New city selector component + warning control function

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
 import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 import * as Location from "expo-location";
+import * as Network from "expo-network";
 
 import bg from "./assets/bg.jpg";
 import loading from "./assets/svgs/loading.svg";
-import {
-  MagnifyingGlassIcon,
-  GlobeEuropeAfricaIcon,
-  MapIcon,
-} from "react-native-heroicons/outline";
-import { CITIES, URL_BASE, URL_FIELDS } from "./constants";
+
+import { URL_BASE, URL_FIELDS } from "./constants";
 
 import Warning from "./components/Warning";
 import CitySelector from "./components/CitySelector";
 import Weather from "./components/Weather";
+import Searcher from "./components/Searcher";
 
 export default function App() {
   const [showCities, setShowCities] = useState(false);
@@ -24,6 +22,8 @@ export default function App() {
   const [fetching, setFetching] = useState(false);
   const [fetchingError, setFetchingError] = useState(false);
   const [dataFetch, setFDataFetch] = useState(null);
+  const [networkStatus, setNetworkStatus] = useState(null);
+  const [isAirplaneModeEnabled, setIsAirplaneModeEnabled] = useState(false);
 
   const fetchAPI = async () => {
     try {
@@ -47,12 +47,16 @@ export default function App() {
   };
 
   useEffect(() => {
+    checkNetworkStatus();
+    checkAirplaneMode();
     selectedLocation && fetchAPI();
   }, [selectedLocation]);
 
   useEffect(() => {
     checkIfLocationEnabled();
     checkIfPermissionGranted();
+    checkNetworkStatus();
+    checkAirplaneMode();
   }, []);
 
   const getLocation = async () => {
@@ -95,74 +99,78 @@ export default function App() {
     }
   };
 
+  const checkNetworkStatus = async () => {
+    setNetworkStatus(await Network.getNetworkStateAsync());
+  };
+
+  const checkAirplaneMode = async () => {
+    setIsAirplaneModeEnabled(await Network.isAirplaneModeEnabledAsync());
+  };
+
+  const checkWarnings = () => {
+    let warnings = [];
+
+    !locationEnabled && // Don't know why is not working properly
+      warnings.push(
+        <Warning key="locationEnabled" message="Location not enabled" />
+      );
+    !permissionGranted &&
+      warnings.push(
+        <Warning
+          key="permissionGranted"
+          message="Location permission not granted"
+        />
+      );
+    fetchingError &&
+      warnings.push(<Warning key="fetchingError" message="Fetching error" />);
+    !networkStatus?.isConnected &&
+      warnings.push(
+        <Warning
+          key="networkStatusIsConnected"
+          message="No internet connection"
+        />
+      );
+    !networkStatus?.isInternetReachable &&
+      warnings.push(
+        <Warning
+          key="networkStatusIsInternetReachable"
+          message="Internet not reachable"
+        />
+      );
+    isAirplaneModeEnabled && // Don't know why is not working properly
+      warnings.push(
+        <Warning
+          key="isAirplaneModeEnabled"
+          message="Airplane mode activated"
+        />
+      );
+
+    return warnings;
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       <Image source={bg} blurRadius={10} style={styles.bg} />
-      <View style={styles.search}>
-        <GlobeEuropeAfricaIcon
-          size={30}
-          style={{ color: "#69bfff", marginHorizontal: 5 }}
-        />
-        <Pressable
-          onPress={() => setShowCities(!showCities)}
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            flex: "1",
-          }}
-        >
-          <Text style={styles.textInput}>
-            {selectedLocation ? selectedLocation.name : "Search city"}
-          </Text>
-          <MagnifyingGlassIcon size={20} style={styles.glassIcon} />
-        </Pressable>
-      </View>
+      <Searcher
+        selectedLocation={selectedLocation}
+        setShowCities={setShowCities}
+        showCities={showCities}
+      />
       {showCities ? (
-        <View
-          style={{
-            marginHorizontal: 20,
-            marginBottom: 10,
-            borderRadius: 10,
-            height: "165px",
-            overflow: "auto",
-          }}
-        >
-          <Pressable
-            style={styles.currLocSelector}
-            onPress={() => {
-              getLocation();
-              setShowCities(false);
-            }}
-          >
-            <MapIcon size={15} style={{ color: "blue", marginRight: 2 }} />
-            <Text style={{ position: "relative", color: "black" }}>
-              My current location
-            </Text>
-          </Pressable>
-          {CITIES.map((city, index) => (
-            <CitySelector
-              key={city.name + index}
-              city={city}
-              index={index}
-              setSelectedLocation={setSelectedLocation}
-              setShowCities={setShowCities}
-            />
-          ))}
-        </View>
+        <CitySelector
+          setSelectedLocation={setSelectedLocation}
+          setShowCities={setShowCities}
+          getLocation={getLocation}
+        />
       ) : null}
       {fetching ? (
-        <View
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
+        <View style={styles.loading}>
           <Image source={loading} style={{ width: 100, height: 100 }} />
         </View>
       ) : (
         <View style={{ marginTop: 10 }}>
-          {!locationEnabled && <Warning message="Location not enabled" />}
-          {!permissionGranted && <Warning message="Permission not granted" />}
-          {fetchingError && <Warning message="Fetching error" />}
+          {checkWarnings()}
           {dataFetch ? (
             <Weather
               place={selectedLocation.name}
@@ -189,41 +197,5 @@ const styles = StyleSheet.create({
     height: "100%",
     width: "100%",
   },
-  search: {
-    position: "relative",
-    display: "flex",
-    flexDirection: "row",
-    margin: 15,
-    marginBottom: 5,
-    padding: 5,
-    borderRadius: 15,
-    borderColor: "#ffffff80",
-    borderWidth: 1,
-    backgroundColor: "#515151db",
-    alignItems: "center",
-  },
-  textInput: {
-    width: "100%",
-    padding: 5,
-    fontSize: 16,
-    color: "white",
-  },
-  glassIconPressable: {
-    alignItems: "center",
-  },
-  glassIcon: {
-    marginRight: 5,
-    padding: 5,
-    color: "white",
-  },
-  currLocSelector: {
-    display: "flex",
-    flexDirection: "row",
-    marginBottom: 1,
-    paddingVertical: 8,
-    paddingLeft: 10,
-    backgroundColor: "#d0fbffe6",
-    color: "#000",
-    gap: 5,
-  },
+  loading: { flex: 1, justifyContent: "center", alignItems: "center" },
 });
--- components/CitySelector.js
+++ components/CitySelector.js
@@ -1,36 +1,56 @@
-import { Text, Pressable, StyleSheet } from "react-native";
-import { MapPinIcon } from "react-native-heroicons/outline";
+import { Text, Pressable, StyleSheet, View } from "react-native";
+import { MapIcon } from "react-native-heroicons/outline";
+import { CITIES } from "../constants";
+import CitySelectorOption from "./CitySelectorOption";
 
 export default function CitySelector({
-  city,
   setSelectedLocation,
   setShowCities,
+  getLocation,
 }) {
   return (
-    <Pressable
-      style={styles.citySelector}
-      onPress={() => {
-        setSelectedLocation(city);
-        setShowCities(false);
-      }}
-    >
-      <MapPinIcon size={15} style={styles.icon} />
-      <Text style={styles.city}>{city.name}</Text>
-    </Pressable>
+    <View style={styles.component}>
+      <Pressable
+        style={styles.currLocSelector}
+        onPress={() => {
+          getLocation();
+          setShowCities(false);
+        }}
+      >
+        <MapIcon size={15} style={{ color: "blue", marginRight: 2 }} />
+        <Text style={{ position: "relative", color: "black" }}>
+          My current location
+        </Text>
+      </Pressable>
+      {CITIES.map((city, index) => (
+        <CitySelectorOption
+          key={city.name + index}
+          city={city}
+          index={index}
+          setSelectedLocation={setSelectedLocation}
+          setShowCities={setShowCities}
+        />
+      ))}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  citySelector: {
+  component: {
+    marginHorizontal: 20,
+    marginBottom: 10,
+    borderRadius: 10,
+    height: "165px",
+    overflow: "auto",
+  },
+  currLocSelector: {
     display: "flex",
     flexDirection: "row",
     marginBottom: 1,
     paddingVertical: 8,
     paddingLeft: 10,
-    backgroundColor: "#f3feffe6",
+    backgroundColor: "#d0fbffe6",
     color: "#000",
     gap: 5,
   },
-  icon: { color: "blue", marginRight: 2 },
-  city: { position: "relative", color: "black" },
 });
